61dcdbe634d0194111b5b50ee47ad1f334b6ee4a
Slawomir Osiak <slawek-osiak@o2.pl>
2017-06-20 18:34:29 +0200

    add chevron right/left

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
 	var carouselList = $("#carousel ul");
-	var changeSlide = carouselList.animate({'marginLeft':-400}, '500', moveFirstSlide);
-	setInterval(changeSlide, 3000);
-	var moveFirstSlide = function() {
+	var interval = setInterval(changeSlide, 5000);
+	
+	function changeSlide () {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	}
+	function moveFirstSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
-		carouselList.css({'marginleft':0});
-	};
+		carouselList.css({marginLeft:0});
+	}
+	function moveLastSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:-400});
+	}
+	$('#prev').click(function() {
+		carouselList.animate({'marginLeft': 400}, 500, moveLastSlide);
+	});
+	$('#next').click(function() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	});
 });
